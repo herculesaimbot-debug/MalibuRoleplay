25c6378cc2e02d9ab2a189664b4f1c588b13add5
herculesaimbot-debug <herculesaimbot@gmail.com>
2026-02-14 20:16:51 -0300

    Update discord-auth.js

--- api/discord-auth.js
+++ api/discord-auth.js
@@ -1,6 +1,6 @@
-import crypto from "crypto";
+const crypto = require("crypto");
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   try {
     const clientId = process.env.DISCORD_CLIENT_ID;
     const redirectUri = process.env.DISCORD_REDIRECT_URI;
@@ -15,10 +15,10 @@ export default async function handler(req, res) {
       "https://discord.com/api/oauth2/authorize" +
       `?client_id=${clientId}` +
       `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-      `&response_type=code` +
+      "&response_type=code" +
       `&scope=${encodeURIComponent("identify email guilds.join")}` +
       `&state=${state}` +
-      `&prompt=consent`;
+      "&prompt=consent";
 
     res.setHeader(
       "Set-Cookie",
@@ -26,8 +26,9 @@ export default async function handler(req, res) {
     );
 
     return res.redirect(url);
+
   } catch (error) {
     console.error(error);
     return res.status(500).send("Internal Server Error");
   }
-}
+};
